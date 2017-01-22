f872ec829150407784ab21f1019ecd07b2cf0640
unknown <robertodavidrobles@gmail.com>
2017-01-22 12:24:02 -0500

    servir archivos estaticos

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require("express");// inportamos el paquete
 var app = express();// inicialixamos el objeto express
 
+//se agrega path virtualprefijo a los archivos estaticos
+app.use("/estatico",express.static('public')); //uso de middlewares
+app.use(express.static('assets'));
+
 app.set("view engine","jade");
 
 app.get("/",function(req,res) {
