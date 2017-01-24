bc50fc5ba2f24c5ba9662de59b27d46d9d4cd1b8
unknown <robertodavidrobles@gmail.com>
2017-01-23 19:47:04 -0500

    se reciben paramentros con body-Parser

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require("express");// inportamos el paquete
 var app = express();// inicialixamos el objeto express
+var bodyParser = require("body-parser");
 
 //se agrega path virtualprefijo a los archivos estaticos
 app.use("/estatico",express.static('public')); //uso de middlewares
+app.use(bodyParser.json());//para peticiones appllication/json
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('assets'));
 
 app.set("view engine","jade");
@@ -15,6 +18,11 @@ app.get("/login",function(req,res) {
   res.render ("login");
 });
 
+app.post("/users",function (req,res) {
+  console.log ("Contraseña: "+req.body.password);//solicitamos lo paramenteor
+  console.log ("Email: "+req.body.email);
+  res.send("se recibio los datos");
+});
 
 
 app.listen(8080);// escuchamos peticiones
